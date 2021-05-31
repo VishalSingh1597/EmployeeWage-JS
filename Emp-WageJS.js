b60673b409d4c65ca3b95ecdc589e9486f4fbe70
VishalSingh1597 <80283079+VishalSingh1597@users.noreply.github.com>
2021-05-31 22:34:42 +0530

    [Vishal]Added: Wage For Month

--- Emp-WageJS.js
+++ Emp-WageJS.js
@@ -10,3 +10,32 @@ else
 {
     console.log("Employee is present");
 }
+
+
+//UC2 Ability to Calculate Daily Employee Wage based on part time or full time work.
+{
+console.log("UC2 Calculating wage for employee");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHours = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+    case IS_PART_TIME:
+    console.log("Employee is working part time");
+    empHours=PART_TIME_HOURS;
+    break;
+    case IS_FULL_TIME:
+    console.log("Employee is working full time");
+    empHours = FULL_TIME_HOURS;
+    break;
+    default:
+    empHours =0;
+    break;
+}
+let empWage = empHours*WAGE_PER_HOUR;
+console.log("Employee wage: " + empWage);
+}
